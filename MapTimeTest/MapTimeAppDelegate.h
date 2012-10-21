8b5dba10982161c8aafadc7c18b65d76a086b81e
Nicholas Angeli <nicangeli@gmail.com>
2012-10-21 16:54:07 +0100

    Initial Commit

---
+++ MapTimeTest/MapTimeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MapTimeAppDelegate.h
+//  MapTimeTest
+//
+//  Created by Nicholas Angeli on 21/10/2012.
+//  Copyright (c) 2012 MapTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapTimeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapTimeTest/MapTimeViewController.m
@@ -0,0 +1,29 @@
+//
+//  MapTimeViewController.m
+//  MapTimeTest
+//
+//  Created by Nicholas Angeli on 21/10/2012.
+//  Copyright (c) 2012 MapTime. All rights reserved.
+//
+
+#import "MapTimeViewController.h"
+
+@interface MapTimeViewController ()
+
+@end
+
+@implementation MapTimeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
