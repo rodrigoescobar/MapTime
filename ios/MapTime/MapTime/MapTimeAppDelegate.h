3641ce7f9f60eff686debfbf498b7bdbd97adfc1
Nicholas Angeli <nicangeli@gmail.com>
2012-10-30 16:03:00 +0000

    Draws line and downloads xml data

---
+++ ios/MapTime/MapTime/MapTimeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MapTimeAppDelegate.h
+//  MapTime
+//
+//  Created by Nicholas Angeli on 30/10/2012.
+//  Copyright (c) 2012 MapTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapTimeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/MapTime/MapTime/MapTimeViewController.m
@@ -0,0 +1,88 @@
+//
+//  MapTimeViewController.m
+//  MapTime
+//
+//  Created by Nicholas Angeli on 30/10/2012.
+//  Copyright (c) 2012 MapTime. All rights reserved.
+//
+
+#import "MapTimeViewController.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface MapTimeViewController ()
+
+@end
+
+@implementation MapTimeViewController
+
+MKMapView *mapView; 
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    mapView = (MKMapView *)[self.view viewWithTag:1001];
+    
+    
+    CLLocationDegrees lat1 = 50.876004;
+    CLLocationDegrees lon1 = -1.373291;
+    CLLocationCoordinate2D coords1 = CLLocationCoordinate2DMake(lat1, lon1);
+    MKMapPoint point1 = MKMapPointForCoordinate(coords1);
+    
+    CLLocationDegrees lat2 = 51.475225;
+    CLLocationDegrees lon2 = -0.131836;
+    CLLocationCoordinate2D coords2 = CLLocationCoordinate2DMake(lat2, lon2);
+    MKMapPoint point2 = MKMapPointForCoordinate(coords2);
+    
+    MKMapPoint *points = malloc(sizeof(CLLocationCoordinate2D)*2);
+    points[0] = point1;
+    points[1] = point2;
+    
+    MKPolyline *line = [MKPolyline polylineWithPoints:points count:2];
+    
+    [mapView addOverlay:line];
+    
+    NSString *xml = [[NSString alloc] initWithString:[self downloadData]];
+}
+
+-(NSString *)downloadData
+{
+    NSURL *url = [NSURL URLWithString:@"http://www.yournavigation.org/api/1.0/gosmore.php?format=kml&flat=52.215676&flon=5.963946&tlat=52.2573&tlon=6.1799&v=motorcar&fast=1&layer=mapnik"];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSHTTPURLResponse *response = nil;
+    NSError *error = nil;
+    
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    if(error != nil) {
+        NSLog(@"%@", [error localizedDescription]);
+    }
+    
+    NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    return string;
+    
+}
+
+
+-(MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id)overlay
+{
+    if([overlay isKindOfClass:[MKPolyline class]]) {
+        MKPolylineView *aView = [[MKPolylineView alloc] initWithPolyline:(MKPolyline *)overlay];
+        aView.strokeColor = [[UIColor blueColor] colorWithAlphaComponent:0.7];
+        return aView;
+    }
+    
+    return nil;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
