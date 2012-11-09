8c664bb26b6e232f5d30cb120520fe0b31bbba1e
ja7g09 <ja7g09@cress.ecs.soton.ac.uk>
2012-11-09 14:31:43 +0000

    Added a Splash-Screen for the application that incorporates the MapTime logo.

--- android/MapTime/gen/com/maptime/maptime/R.java
+++ android/MapTime/gen/com/maptime/maptime/R.java
@@ -13,12 +13,15 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int splash_logo=0x7f020002;
     }
     public static final class id {
+        public static final int imageView1=0x7f070004;
         public static final int mapview=0x7f070000;
-        public static final int menu_refresh=0x7f070006;
-        public static final int menu_startnav=0x7f070005;
-        public static final int menu_timelines=0x7f070004;
+        public static final int menu_refresh=0x7f070007;
+        public static final int menu_settings=0x7f070008;
+        public static final int menu_startnav=0x7f070006;
+        public static final int menu_timelines=0x7f070005;
         public static final int tlcList=0x7f070003;
         public static final int tlcMain=0x7f070001;
         public static final int tlcTXT=0x7f070002;
@@ -26,16 +29,20 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_timelinechoice=0x7f030001;
+        public static final int splash_screen=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
         public static final int activity_timelinechoice=0x7f060001;
+        public static final int splash_screen=0x7f060002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int error_readXML=0x7f040009;
         public static final int error_title=0x7f040008;
+        public static final int hello_world=0x7f04000c;
         public static final int menu_refresh=0x7f040002;
+        public static final int menu_settings=0x7f04000d;
         public static final int menu_startnav=0x7f040005;
         public static final int menu_timelines=0x7f040001;
         public static final int progress_calculating=0x7f04000a;
@@ -43,6 +50,7 @@ public final class R {
         public static final int progress_fetchingTimlines=0x7f040007;
         public static final int progress_loading=0x7f040006;
         public static final int title_activity_main=0x7f040003;
+        public static final int title_activity_splash_screen=0x7f04000e;
         public static final int title_activity_timelinechoice=0x7f040004;
     }
     public static final class style {
