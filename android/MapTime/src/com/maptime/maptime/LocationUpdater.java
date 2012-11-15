ccaf81fd9021492e96c1eb3a5541a3d25bbe8f7b
npadoin <ncp1g09@ecs.soton.ac.uk>
2012-11-15 20:46:41 +0000

    Location now properly retrieved and updated, but only from GPS right now.

---
+++ android/MapTime/src/com/maptime/maptime/LocationUpdater.java
@@ -0,0 +1,35 @@
+package com.maptime.maptime;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Basically a dummy class used to make the app actually get location updates.
+ * @author Nathan
+ *
+ */
+
+public class LocationUpdater implements LocationListener{
+
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
