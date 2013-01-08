ca41cb962aa8318fc25ac920f62612c5f0987143
Nicholas Angeli <nicangeli@gmail.com>
2013-01-08 20:54:43 +0000

    adding detail view controller

---
+++ ios/MapTime/MapTime/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  MapTime
+//
+//  Created by Nicholas Angeli on 08/01/2013.
+//  Copyright (c) 2013 MapTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
